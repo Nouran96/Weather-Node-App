480a6df06d33bb6d645cee8967f93a1a8962d149
Nouran <nourans.96@yahoo.com>
2020-03-19 16:06:53 +0200

    Added header and footer partials to be used in all pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,22 +1,26 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
 // Define paths for Express configuration
 const publicDir = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebars engine and views location
 app.set('view engine', 'html') // for dynamic templates
 app.engine('html', require('hbs').__express); // remove this if templates have extension .hbs
 app.set('views', viewsPath); // change the default views directory to templates
+hbs.registerPartials(partialsPath); // for partial templates
 
 app.use(express.static(publicDir)) // for static files
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather'
+        title: 'Weather Page',
+        name: 'Nouran Samy'
     });
 })
 
@@ -29,7 +33,9 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        message: 'This is the help message'
+        message: 'This is the help message',
+        title: 'Help Page',
+        name: 'Nouran Samy'
     })
 })
 
