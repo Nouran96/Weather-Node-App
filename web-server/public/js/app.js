37cd25d98bd64cbb024892325f058e96e75cc297
Nouran <nourans.96@yahoo.com>
2020-03-19 22:00:26 +0200

    Viewed weather data to client asynchronously

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,16 +1,31 @@
 const weatherBtn = document.querySelector('#getWeatherBtn');
 const addressInput = document.querySelector('#addressInput');
+const errorSpan = document.querySelector('#errorMsg');
+const form = document.querySelector('form');
+const locationEl = document.querySelector('#location');
+const forecastEl = document.querySelector('#forecast');
 
 weatherBtn.onclick = (ev) => {
     ev.preventDefault();
-    
+
+    errorSpan.style.visibility = 'visible'
+    errorSpan.textContent = 'Loading ...';
+    locationEl.textContent = '';
+    forecastEl.textContent = '';
+
     const address = addressInput.value;
+
     fetch(`/weather?address=${address}`).then((res) => {
         return res.json()
     }).then(data => {
         if(data.error) {
-            console.log(data.error)
+            errorSpan.textContent = data.error;
         } else {
+            errorSpan.textContent = ''
+
+            locationEl.textContent = data.location;
+            forecastEl.textContent = data.forecast;
+            
             console.log(data.location)
             console.log(data.forecast)
         }
