8fa2f078207cc4c1589980281d1ff22c18cb75d6
Nouran <nourans.96@yahoo.com>
2020-03-19 13:47:13 +0200

    Added web server using express

---
+++ web-server/src/app.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+// index.html is the default page for root url
+const publicDir = path.join(__dirname, '../public');
+
+app.use(express.static(publicDir))
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'My Forecast',
+        location: 'My location'
+    });
+});
+
+app.listen(8000, () => {
+    console.log('Server is listening')
+})
