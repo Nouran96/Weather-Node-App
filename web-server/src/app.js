2ad649b6888881f59f770330decf7052821a700c
Nouran <nourans.96@yahoo.com>
2020-03-19 15:24:07 +0200

    Used handlebars module to add dynamic templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,7 +6,30 @@ const app = express();
 // index.html is the default page for root url
 const publicDir = path.join(__dirname, '../public');
 
-app.use(express.static(publicDir))
+app.set('view engine', 'html') // for dynamic templates
+app.engine('html', require('hbs').__express); // remove this templates have extension .hbs
+
+app.use(express.static(publicDir)) // for static files
+
+app.get('', (req, res) => {
+    // templates should be added in views folder
+    res.render('index', {
+        title: 'Weather'
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Page',
+        name: 'Nouran Samy'
+    });
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'This is the help message'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
